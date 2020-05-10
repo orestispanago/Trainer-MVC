382e32a0a42cfc5f1701543b1b35e7f592feef15
orestis <orestispanago@gmail.com>
2020-05-11 00:35:07 +0300

    Added bootstrap in trainerslist, fixed update link

--- src/main/java/org/panago/trainermvc/controllers/TrainerController.java
+++ src/main/java/org/panago/trainermvc/controllers/TrainerController.java
@@ -12,80 +12,79 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 @Controller
 @RequestMapping("/")
 public class TrainerController {
+
     private String listurl = "list";
     private String editurl = "edit";
     private String deleteurl = "delete";
     private String updateurl = "update";
-    
+
     @Autowired
     ITrainer trainerService;
-    
+
     @Autowired
     MessageSource messageSource;
-    
+
     // index
-    @RequestMapping(value = { "/", "/list" }, method = RequestMethod.GET)
+    @RequestMapping(value = {"/", "/list"}, method = RequestMethod.GET)
     public String listAllTrainers(ModelMap view, @RequestParam(required = false) String msg) {
-        List<Trainer> trainers  = trainerService.findAllTrainers();
+        List<Trainer> trainers = trainerService.findAllTrainers();
         view.addAttribute("trainers", trainers);
         view.addAttribute("editurl", editurl);
         view.addAttribute("deleteurl", deleteurl);
         view.addAttribute("msg", msg);
-        return("trainerlist");
+        return ("trainerlist");
     }
-    
+
     // get form for new trainer
     @RequestMapping("/new")
     public String newTrainer(ModelMap view) {
         Trainer trainer = new Trainer();
         view.addAttribute("trainer", trainer);
         view.addAttribute("listurl", listurl);
-        return("newtrainer");
+        return ("newtrainer");
     }
-    
+
     // post form for new trainer
     @RequestMapping(value = "/new", method = RequestMethod.POST)
     public String saveTrainer(ModelMap view, Trainer trainer) {
-        if(trainerService.save(trainer)) {
+        if (trainerService.save(trainer)) {
             view.addAttribute("message", new String("All good!"));
-        }
-        else {
+        } else {
             view.addAttribute("message", new String("All wrong!"));
         }
         view.addAttribute("listurl", listurl);
-        return("newtrainer");
+        return ("newtrainer");
     }
-    
+
     // delete for an existing trainer
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
     public String deleteTrainer(ModelMap view, @PathVariable int id) {
-        if(trainerService.delete(id)) {
+        if (trainerService.delete(id)) {
             view.addAttribute("msg", new String("Deleted Successfully!"));
         } else {
             view.addAttribute("msg", new String("Not Deleted!"));
         }
-        return("redirect:/list");
+        return ("redirect:/list");
     }
-    
+
     // show edit / update for an existing trainer
     @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
     public String editTrainer(ModelMap view, @PathVariable int id) {
         Trainer trainer = trainerService.findById(id);
         view.addAttribute("trainer", trainer);
         view.addAttribute("updateurl", updateurl);
-        return("edittrainer");
+        return ("edittrainer");
     }
-    
+
     // store edit / update for an existing trainer
     @RequestMapping(value = "/update", method = RequestMethod.POST)
     public String updateTrainer(ModelMap view, Trainer trainer) {
         trainerService.update(trainer);
         view.addAttribute("msg", new String(""));
-        return("redirect:/list");
+        return ("redirect:/list");
     }
-    
+
 }
--- src/main/webapp/WEB-INF/views/trainerlist.jsp
+++ src/main/webapp/WEB-INF/views/trainerlist.jsp
@@ -4,25 +4,55 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+              crossorigin="anonymous">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+              integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
         <title>Trainers' List</title>
     </head>
     <body>
         <h1>Trainers' List</h1>
-        <table>
-		<tr>
-                    <td>ID</td><td>FIRST NAME</td><td>LAST NAME</td><td>SUBJECT</td><td></td>
-		</tr>
-		<c:forEach items="${trainers}" var="trainer">
-			<tr>
-                            <!-- edit: http://localhost:8081/edit/{id} -->
-                            <td><a href="<c:url value='${editurl}/${trainer.id}' />">${trainer.id}</a></td>
+        <div class="container-fluid" id="navbarid">
+        </div>
+        <div id="includedContent"></div>
+        <!--<h1>Trainers</h1>-->
+        <div class="container">
+            <table class="table table-hover">
+                <thead class="bg-primary text-white">
+                    <tr>
+                        <th>First name</th>
+                        <th>Last name</th>
+                        <th>Subject</th>
+                        <th class="text-right"> Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <c:forEach items="${trainers}" var="trainer">
+                        <tr>
                             <td>${trainer.firstName}</td>
                             <td>${trainer.lastName}</td>
                             <td>${trainer.subject}</td>
-                            <td><a href="<c:url value='${deleteurl}/${trainer.id}' />">delete</a></td>
-			</tr>
-		</c:forEach>
-	</table>
+                            <td align="right">
+                                <a href="<c:url value='${editurl}/${trainer.id}' />" class="btn btn-warning">
+                                    <span class="fa fa-pencil"></span> Update
+                                </a>
+                                <a href="<c:url value='${deleteurl}/${trainer.id}' />" class="btn btn-danger">
+                                    <span class="fa fa-trash"></span> Delete
+                                </a>
+                            </td>
+                        </tr>
+                    </c:forEach>
+
+                </tbody>
+
+            </table>
+            <a href="new" class="btn btn-success">
+                <span class="fa fa-plus"></span> Add
+            </a>
+        </div>
+
         <div id="msg">${msg}</div>
     </body>
 </html>
